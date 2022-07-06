57da18044c635c5f2213b5aab00f84233e7129a0
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-07-06 17:17:11 -0300

    add method that retrieve all comments by topic id

---
+++ src/java/Model/comentarioDAO.java
@@ -0,0 +1,48 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class comentarioDAO {
+    
+    static{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }    
+    }
+    
+    public List<comentarioBean> todosComentarios(String login){
+        List<comentarioBean> todos = new ArrayList<>();
+        
+        try(Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/usuarioDB", "usuarioTeste", "senhaPadrao")){
+            String sql = "SELECT * FROM comentario "
+                    + "INNER JOIN topico ON (com_top_codigo = top_codigo)"
+                    + "INNER JOIN usuario ON (top_usu_login = login)"
+                    + "WHERE top_usu_login = ?";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setString(1, login);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                todos.add(montaObjeto(rs));
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return todos;
+    }
+    
+    private comentarioBean montaObjeto(ResultSet rs) throws SQLException{
+        usuarioDAO u = new usuarioDAO();
+        topicosDAO t = new topicosDAO();
+        return new comentarioBean(String.valueOf(rs.getInt("com_codigo")), rs.getString("com_comentario"), u.montaObjeto(rs), t.montaObjeto(rs));
+    }
+}
