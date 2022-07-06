303cb4647c1e3e1a821655131bebee7be1fbc250
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-07-06 13:41:00 -0300

    add page for successful login

---
+++ src/java/controller/Controller.java
@@ -0,0 +1,33 @@
+package controller;
+
+import Model.usuarioDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Controller", urlPatterns = {"/autenticar"})
+public class Controller extends HttpServlet {
+
+    usuarioDAO dao = new usuarioDAO();    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        switch(request.getServletPath()){
+            
+            case "/autenticar":
+                String nome = dao.autenticar(request.getParameter("login"), request.getParameter("senha"));
+                request.setAttribute("nome", nome);
+                if(!nome.equalsIgnoreCase("Erro")){
+                    request.getRequestDispatcher("jsp/topicos.jsp").forward(request, response);
+                }
+        }
+    }
+}
---
+++ web/jsp/topicos.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Anek+Malayalam:wght@400;700&display=swap" rel="stylesheet">
+<title>Tópicos</title>
+</head>
+<body>
+	<h1>Bem Vindo ${nome}</h1>
+</body>
+</html>
