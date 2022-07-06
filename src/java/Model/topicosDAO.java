7b317949e4269749e89d64d50edbad4d819c7224
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-07-06 15:26:59 -0300

    add method to return a list of all topics related to an especific user

--- src/java/Model/topicosDAO.java
+++ src/java/Model/topicosDAO.java
@@ -1,6 +1,45 @@
 
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class topicosDAO {
     
+    static{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }    
+    }
+    
+    public List<topicoBean> todosTopicos(String login){
+        List<topicoBean> todos = new ArrayList<>();
+        
+        try(Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/usuarioDB", "usuarioTeste", "senhaPadrao")){
+            String sql = "SELECT * FROM topico WHERE top_usu_login = ?";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setString(1, login);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                todos.add(montaObjeto(rs));
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return todos;
+    }
+    
+    private topicoBean montaObjeto(ResultSet rs)throws SQLException{
+        return new topicoBean(String.valueOf(rs.getInt("top_codigo")), rs.getString("top_titulo"), rs.getString("top_conteudo"), rs.getString("top_usu_login"));
+    }
 }
