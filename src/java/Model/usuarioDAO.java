4e3ba89c8d9ff699455ab0cf1356bc7080aea4d2
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-07-06 14:46:46 -0300

    add register method

--- src/java/Model/usuarioDAO.java
+++ src/java/Model/usuarioDAO.java
@@ -35,4 +35,20 @@ public class usuarioDAO {
         
         return "Erro";
     }
+    
+    public void cadastrar(usuarioBean u){
+        try(Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1.3306/usuarioDB", "usuarioTeste", "senhaPadrao")){
+            String sql = "INSERT INTO usuario(login, nome, email, senha, pontos) VALUES(?, ?, ?, ?, 0)";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            
+            stmt.setString(1, u.getLogin());
+            stmt.setString(2, u.getNome());
+            stmt.setString(3, u.getEmail());
+            stmt.setString(4, u.getSenha());
+            stmt.executeUpdate();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
